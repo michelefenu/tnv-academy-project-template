0a677b0108064b0830d30b7ce1cf11c1abd20933
ValentinaFarci <valentina.farci85@gmail.com>
2022-06-29 00:25:18 +0200

    prima versione welcome page

--- Angular/src/app/components/welcome/welcome.component.ts
+++ Angular/src/app/components/welcome/welcome.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  imagePath: string="assets/images/welcome.jpg"
   constructor() { }
 
   ngOnInit(): void {
