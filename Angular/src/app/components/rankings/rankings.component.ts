4885b9f8095c5fdf7387e3af9b816354ade4003e
ValentinaFarci <valentina.farci85@gmail.com>
2022-06-29 18:21:06 +0200

    classifica dinamica

--- Angular/src/app/components/rankings/rankings.component.ts
+++ Angular/src/app/components/rankings/rankings.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Posizione } from "src/app/@models/classifica";
 
 @Component({
-  selector: 'tnv-rankings',
-  templateUrl: './rankings.component.html',
-  styleUrls: ['./rankings.component.scss']
+  selector: "tnv-rankings",
+  templateUrl: "./rankings.component.html",
+  styleUrls: ["./rankings.component.scss"],
 })
 export class RankingsComponent implements OnInit {
+  classifica: Posizione[] = [
+    { position: 1, username: "string", titleFilm: "string", timeSpend: 2 },
+    { position: 2, username: "string", titleFilm: "string", timeSpend: 12 },
+    { position: 3, username: "string", titleFilm: "string", timeSpend: 22 },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
