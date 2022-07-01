c937577ac3263a8340491afbb11b89506f2a7e25
ValentinaFarci <valentina.farci85@gmail.com>
2022-07-01 21:00:43 +0200

    fix ngFor

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,22 +3,21 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './@shared/components/login/login.component';
-import { LogoutComponent } from './@shared/components/logout/logout.component';
-import { RegisterComponent } from './@shared/components/register/register.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
-import { NavbarComponent } from './@shared/components/navbar/navbar.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { RankingsComponent } from './components/rankings/rankings.component';
-import { FooterMGComponent } from './footer-mg/footer-mg.component';
-import { GamePageComponent } from './game-page/game-page.component';
-import { CommentSectionComponent } from './comment-section/comment-section.component';
-
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { LoginComponent } from "./@shared/components/login/login.component";
+import { LogoutComponent } from "./@shared/components/logout/logout.component";
+import { RegisterComponent } from "./@shared/components/register/register.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { NavbarComponent } from "./@shared/components/navbar/navbar.component";
+import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { RankingsComponent } from "./components/rankings/rankings.component";
+import { FooterMGComponent } from "./footer-mg/footer-mg.component";
+import { GamePageComponent } from "./game-page/game-page.component";
+import { CommentSectionComponent } from "./comment-section/comment-section.component";
+import { FavoritesComponent } from "./components/favorites/favorites.component";
 
 @NgModule({
   declarations: [
@@ -34,6 +33,7 @@ import { CommentSectionComponent } from './comment-section/comment-section.compo
     FooterMGComponent,
     GamePageComponent,
     CommentSectionComponent,
+    FavoritesComponent,
   ],
   imports: [
     BrowserModule,
--- Angular/src/app/components/favorites/favorites.component.ts
+++ Angular/src/app/components/favorites/favorites.component.ts
@@ -1,36 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Favoriti } from '../../@models/favoriti';
-import { FavoritesService } from '../../@service/favorites.service';
+import { Component, OnInit } from "@angular/core";
+import { Favoriti } from "../../@models/favoriti";
+import { FavoritesService } from "../../@service/favorites.service";
 
 @Component({
-  selector: 'app-favorites',
-  templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss'],
+  selector: "app-favorites",
+  templateUrl: "./favorites.component.html",
+  styleUrls: ["./favorites.component.scss"],
 })
 export class FavoritesComponent implements OnInit {
-  filterText: string = '';
-
   favorites: Favoriti[] = [];
-  filteredFavorites: Favoriti[] = [];
-  categories: string[] = [];
-  filteredfavorites: any;
 
   constructor(private favoritesService: FavoritesService) {}
 
   ngOnInit(): void {
-    const getMenuObservable = this.favoritesService.getFavorites();
+    const getObservable = this.favoritesService.getFavorites();
 
-    getMenuObservable.subscribe({
+    getObservable.subscribe({
       next: (favoriti: Favoriti[]) => {
-       console.log (favoriti)
+        this.favorites = favoriti;
       },
+      error: (err) => console.error(err),
     });
   }
-
-  getSectionData(category: string) {
-    return this.filteredfavorites.filter((x: { category: string; }) => x.category === category);
-  }
-
-
-
 }
