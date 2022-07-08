3c95fc5a8af5ea3e24eea17713c959b678b55efe
ValentinaFarci <valentina.farci85@gmail.com>
2022-07-08 14:38:01 +0200

    dettaglio

--- Node/app/src/controllers/ratings-controller.js
+++ Node/app/src/controllers/ratings-controller.js
@@ -39,6 +39,21 @@ export const getAllRating = async (req, res) => {
   }
 };
 
+export const getRatingById = async (req, res) => {
+  try {
+    const rating = await Rating.findByPk(req.params.id);
+
+    if (rating) {
+      res.send(rating);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+};
+
 export const createRating = async (req, res) => {
   try {
     const rating = await Rating.create(req.body);
--- Node/app/src/routes/routes.js
+++ Node/app/src/routes/routes.js
@@ -8,11 +8,13 @@ import {
   createRating,
   updateRating,
   deleteRating,
+  getRatingById,
 } from "../controllers/ratings-controller.js";
 
 const router = express.Router();
 
 router.get(`${API_ROOT}/rating`, getAllRating);
+router.get(`${API_ROOT}/rating/:id`, getRatingById);
 router.get(`${API_ROOT}/favorites/:userId/`, getFavoritesByUserId);
 router.post(`${API_ROOT}/rating`, createRating);
 router.patch(`${API_ROOT}/rating/:id`, updateRating);
