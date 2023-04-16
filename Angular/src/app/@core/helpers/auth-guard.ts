f7f350f55d77175f7fbad88a9a81e121655c7e9d
Michele Fenu <michelefenu@gmail.com>
2023-04-16 21:11:27 +0200

    Remove deprecated interface

--- Angular/src/app/@core/helpers/auth-guard.ts
+++ Angular/src/app/@core/helpers/auth-guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
-import { Router, CanActivate } from "@angular/router";
+import { Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(public auth: AuthService, public router: Router) {}
 
   canActivate(): boolean {
