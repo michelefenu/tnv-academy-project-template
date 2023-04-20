8e8d84305bbf736b1c01d16c08749a53f3ddbe74
GabrielStanciu0314 <128413715+GabrielStanciu0314@users.noreply.github.com>
2023-04-20 12:02:46 +0200

    Disegnato search component

--- Angular/src/app/@shared/modules/angular-material/angular-material.module.ts
+++ Angular/src/app/@shared/modules/angular-material/angular-material.module.ts
@@ -7,6 +7,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle'; 
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   exports: [
@@ -16,7 +17,8 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatSelectModule
   ],
   providers: [
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
