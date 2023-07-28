9a393e94c6d30cafce635d7331268fdc29d55f25
AndreaLoviselli <andrealoviselli19@gmail.com>
2023-07-28 11:50:52 +0200

    commit gioca

--- Angular/src/app/components/play/play.component.ts
+++ Angular/src/app/components/play/play.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { TmdService } from 'src/app/@shared/servicesTMD/tmd.service';
+
 
 @Component({
   selector: 'tnv-play',
@@ -9,16 +11,40 @@ import { TmdService } from 'src/app/@shared/servicesTMD/tmd.service';
 
 export class PlayComponent implements OnInit{
 
-
+// 
+  val = {
+    movieTitleAttempt: ""
+  };
+//
+//
   movieId: number;
   idFamousMovies: number[];
+
 
   showOverview: boolean = false;
   showReleaseDate: boolean = false;
   showVoteAverage: boolean = false;
   showOriginalLanguage: boolean = false;
   showPopularity: boolean = false;
+//
 
+//
+  totalElapsedTime = 0;
+  isRunning = false;
+  timeIncrement = 30;
+  cardElapsedTime: number = 0;
+  totalTimeEvent = new EventEmitter<number>();
+
+  private intervalId: any;
+//
+
+//
+clickedBtOne: boolean = false;
+clickedBtTwo: boolean = false;
+clickedBtThree: boolean = false;
+clickedBtFour: boolean = false;
+clickedBtFive: boolean = false;
+//
 
 
   
@@ -26,35 +52,81 @@ export class PlayComponent implements OnInit{
   constructor(public tmdService: TmdService){
     this.idFamousMovies = [ 550, 346698, 298618, 447365, 667538, 447277, 872585, 502356, 569094, 603692, 385687, 921636, 921636];
     this.movieId = this.idFamousMovies[Math.floor(Math.random() * this.idFamousMovies.length)];
-    
-    console.log(this.movieId)
+
+
   }
 
   ngOnInit(): void {
     this.tmdService.getMovie(this.movieId)
     
-    
-
   }
+
+  //
+  startTimer(){
+    this.isRunning= true;
+    this.intervalId = setInterval(() => {
+      this.totalElapsedTime++;
+    }, 1000);
+  }
+
+  stopTimer(){
+    this.isRunning = false;
+    clearInterval(this.intervalId);
+    this.totalTimeEvent.emit(this.totalElapsedTime + this.cardElapsedTime);
+}
+
+  addTime(){
+    this.totalElapsedTime += this.timeIncrement;
+  }
+
+  restartTimer(){
+    this.totalElapsedTime = 0;
+    this.cardElapsedTime = 0;
+    this.isRunning = false;
+    clearInterval(this.intervalId);
+  }
+  //
 
   onClickButtonShowOverview(){
     this.showOverview = true;
+    this.addTime()
+    this.clickedBtOne = true;
   }
 
   onClickButtonShowReleaseDate(){
     this.showReleaseDate = true;
+    this.addTime()
+    this.clickedBtTwo = true;
+
   }
 
   onClickButtonShowVoteAverage(){
     this.showVoteAverage = true;
+    this.addTime()
+    this.clickedBtThree = true;
   }
 
   onClickButtonShowOriginalLanguage(){
     this.showOriginalLanguage = true;
+    this.addTime()
+    this.clickedBtFour = true;
   }
 
   onClickButtonShowPopularity(){
     this.showPopularity = true;
+    this.addTime()
+    this.clickedBtFive = true;
+  }
+
+  
+  checkIfMovieCorrect(guessMovieForm: NgForm, movieTitle: string){
+    console.log(movieTitle)
+    console.log(this.val.movieTitleAttempt)
+    if(this.val.movieTitleAttempt === movieTitle){
+      console.log("bravo hai vinto")
+      this.stopTimer();
+    }
+
   }
 }
 
