63002af111f6ea660cba33394ca1ac933ca92a50
LucaScalas <lucascalas6153@gmail.com>
2023-12-21 16:05:07 +0100

    aggiunta springboot

--- SpringBoot/sbTutorial1-JPA-Security2/src/main/java/com/thenetvalue/sbTutorial1/model/User.java
+++ SpringBoot/sbTutorial1-JPA-Security2/src/main/java/com/thenetvalue/sbTutorial1/model/User.java
@@ -12,9 +12,11 @@ public class User {
     @SequenceGenerator(name = "userSequence", sequenceName = "user_seq", allocationSize = 1)
     private int id;
     @Column()
+    private String name;
+    private String surname;
     private String username;
     private String password;
-    private String email;
+    //private String email;
     private boolean enabled = true;
 
     public int getId() {
@@ -41,13 +43,37 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* public String getEmail() {
         return this.email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
+    }*/
 
 //    public boolean isEnabled() {
 //        return enabled;
