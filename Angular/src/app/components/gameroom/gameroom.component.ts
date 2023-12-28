bf4fcaeba6ef36dc1de48be14f9f966caf965b5c
AlessioSanna <noticebox.job@outlook.com>
2023-12-28 18:46:55 +0100

    ng g c gameroom

---
+++ Angular/src/app/components/gameroom/gameroom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CountdownModule } from 'ngx-countdown';
+
+@Component({
+  selector: 'tnv-gameroom',
+  templateUrl: './gameroom.component.html',
+  styleUrl: './gameroom.component.scss'
+})
+export class GameroomComponent {
+
+}
