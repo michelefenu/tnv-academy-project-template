c431c74ce09596ae996f9d8be4b5249006c886fa
AlessioSanna <noticebox.job@outlook.com>
2024-01-02 12:47:59 +0100

    timer component UI

--- Angular/src/app/components/timer/timer.component.ts
+++ Angular/src/app/components/timer/timer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'tnv-timer',
   templateUrl: './timer.component.html',
-  styleUrl: './timer.component.scss'
+  styleUrls: ['./timer.component.scss']
 })
 
 export class TimerComponent implements OnDestroy {
@@ -28,7 +28,7 @@ export class TimerComponent implements OnDestroy {
         this.counter = elapsedTime;
       });
     } else {
-      this.startText = 'Resume';
+      this.startText = 'Start';
       if (this.timerRef !== undefined) {
         clearInterval(this.timerRef);
       }
@@ -50,10 +50,18 @@ export class TimerComponent implements OnDestroy {
     const seconds = totalSeconds % 60;
     const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
     const decimalPart = (milliseconds % 1000) / 1000;
-  
-    return `${minutes}:${formattedSeconds} ${decimalPart.toFixed(2).slice(4)}`;
+
+    return `${minutes}:${formattedSeconds} ${decimalPart.toFixed(2).slice(4)}`;   //qui è possibile ridurre le cifre cronometro in scorrimento: .slice(2)= 0.00 000 ----> con millisecondi
   }
-  
+
+  newCardQuestion() {
+    // Implementa la logica della funzione newCardQuestion qui
+    console.log('Nuova domanda inserita!');
+  }
+
+  setUserPoints(){
+    //implementare funzione che incrementa un punteggio di un user
+  }
 
   ngOnDestroy() {
     if (this.timerRef !== undefined) {
