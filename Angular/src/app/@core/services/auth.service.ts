999d0c5decf6bf7bf2410d41aca51bcb68d3c57d
AlessioSanna <noticebox.job@outlook.com>
2024-01-04 11:03:19 +0100

    connesso springboot e DB in auth.service.ts e in node

--- Angular/src/app/@core/services/auth.service.ts
+++ Angular/src/app/@core/services/auth.service.ts
@@ -3,56 +3,61 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { of } from "rxjs";
 import { LoginDTO, RegisterDTO, User } from "src/app/models/user";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  springBootUrl = 'http://localhost:8080';
+  springBootUrl = 'http://localhost:8080/users';
 
   constructor(private router: Router, private http: HttpClient) {}
 
-  login(loginData: LoginDTO) {
-    console.log('auth service.ts', loginData);
+  login(loginData: LoginDTO) : Observable<any> {
 
-    // Passare username e password
-    // return this.http.get(`${this.springBootUrl}/api/user`);
-
-    // Stub prima di implementare l'API
-    const user: User = {
-      name: 'Paolino',
+       const user: User = {
+        name: 'Paolino',
       surname: 'Paperino',
       username: 'papero123'
     }
     return of(user);
-    // Fine stub
+    // Fine stub 
   }
+    
+ /*    console.log('auth service.ts', loginData);
+
+    const url = `${this.springBootUrl}/login`; // Sostituisci con il tuo endpoint effettivo per il login
+    return this.http.post(url, loginData);
+
+    const user = loginData;
+
+     */
+
+   
+
+    // Passare username e password
+    // return this.http.get(${this.springBootUrl}/api/user);
+
+    // Stub prima di implementare l'API
+ /*      const user: User = {
+        name: 'Paolino',
+      surname: 'Paperino',
+      username: 'papero123'
+    }
+    return of(user);
+    // Fine stub 
+  } */
 
   register(registerData: RegisterDTO) {
-     // Effettua la chiamata HTTP al backend per la registrazione
-     this.http.post(`${this.springBootUrl}/api/register`, registerData).subscribe({
-      next: (response: any) => {
-        // Se la registrazione ha successo, reindirizza l'utente alla pagina desiderata dopo il login
-        this.router.navigateByUrl("/login");
-      },
-      error: (error) => {
-        console.error("Errore durante la registrazione:", error);
-        // Gestire eventuali errori durante la registrazione qui
+    console.log('auth service.ts', registerData);
 
-   /* console.log('auth service.ts', registerData);
     const url = `${this.springBootUrl}/`; // Sostituisci con il tuo endpoint effettivo per il login
-    return this.http.post(url, registerData);*/
-      }
-    });
-  }
+    return this.http.post(url, registerData);
 
-/*   versione originale progetto
-  register(registerData: RegisterDTO) {
 
     // TODO Chiamare il servizio per la registrazione e redirigere l'utente alla root per il login
     //this.router.navigateByUrl("/");
   }
- */
 
   logout() {
     localStorage.removeItem("user");
