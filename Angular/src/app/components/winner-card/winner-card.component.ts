f9a84e350103d6c8a5f9c4e84d9897bd709231ac
AlessioSanna <noticebox.job@outlook.com>
2024-01-04 21:35:49 +0100

    modifiche UI winner-card

---
+++ Angular/src/app/components/winner-card/winner-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tnv-winner-card',
+  templateUrl: './winner-card.component.html',
+  styleUrl: './winner-card.component.scss'
+})
+export class WinnerCardComponent {
+
+}
