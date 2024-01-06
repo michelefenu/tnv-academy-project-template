7c632f6af8b050c8da64f790ee94fa36c7dff955
simoc7 <simcs.476@gmail.com>
2024-01-06 22:49:55 +0100

    route winner-card, implementazione di prova timer

--- Angular/src/app/components/winner-card/winner-card.component.ts
+++ Angular/src/app/components/winner-card/winner-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieService } from 'src/app/@core/services/movie.service';
+import { TimerService } from 'src/app/@core/services/timer.service';
 
 @Component({
   selector: 'tnv-winner-card',
   templateUrl: './winner-card.component.html',
   styleUrl: './winner-card.component.scss'
 })
-export class WinnerCardComponent {
+export class WinnerCardComponent implements OnInit {
+
+  constructor(private router:Router, private movieService:MovieService, private timerService:TimerService){};
+
+  ngOnInit():void{
+
+    this.movieService.getMovieDetailsById
+
+
+  }
+
 
 }
