0ec1cc647254bdcf88ff9cde8c2740ff5e5cf4d8
simoc7 <simcs.476@gmail.com>
2024-01-07 09:30:29 +0100

    aggiunta dati film su winner-card

--- Angular/src/app/components/winner-card/winner-card.component.ts
+++ Angular/src/app/components/winner-card/winner-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MovieService } from 'src/app/@core/services/movie.service';
 import { TimerService } from 'src/app/@core/services/timer.service';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'tnv-winner-card',
@@ -9,12 +10,22 @@ import { TimerService } from 'src/app/@core/services/timer.service';
   styleUrl: './winner-card.component.scss'
 })
 export class WinnerCardComponent implements OnInit {
+  movie: Movie | undefined;
+  poster: string | undefined;
+  imageurl: string = 'https://image.tmdb.org/t/p/w500';
+  title!: string; 
+
 
   constructor(private router:Router, private movieService:MovieService, private timerService:TimerService){};
 
   ngOnInit():void{
 
-    this.movieService.getMovieDetailsById
+   this.movie = this.movieService.getSelectedMovie();
+   this.poster = this.imageurl + this.movie.poster_path;
+   console.log("poster",this.poster);
+   this.title = this.movie.title;
+
+   
 
 
   }
